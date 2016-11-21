515bb0e4913ecfdfef63c5d0e6377bd6ce3cc119
Ivan Gromakovskii <gie1994@gmail.com>
2016-11-22 02:11:37 +0300

    [CSL-24] Remove sscProcessMessage from SscLocalDataClass

--- src/Pos/Ssc/Class/LocalData.hs
+++ src/Pos/Ssc/Class/LocalData.hs
@@ -13,7 +13,5 @@ import           Pos.Types.Types     (SlotId)
 class Ssc ssc => SscLocalDataClass ssc where
     sscNewLocalData :: MonadIO m => m (SscLocalData ssc)
     sscDestroyLocalData :: MonadIO m => SscLocalData ssc -> m ()
-    -- first argument is global payload which is taken from main persistent storage
-    sscProcessMessage :: MonadIO m => SscPayload ssc -> SscMessage ssc -> SscLocalData ssc -> m ()
     -- maybe should take global payload too
     sscGetLocalPayload :: MonadIO m => SlotId -> SscLocalData ssc -> m (SscPayload ssc)
